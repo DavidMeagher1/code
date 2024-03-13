d6113bc858ca93e2de3f47134ee0136d845f6126
DESKTOP-7J59REQ\meagh <meagherjdavid@gmail.com>
2024-03-13 09:31:40 -0700

    hex parsing & removed tokens:
    changing to making a simple hex string parser initially

--- src/main.zig
+++ src/main.zig
@@ -8,50 +8,6 @@ const File = fs.File;
 
 const FixedBufferAllocator = heap.FixedBufferAllocator;
 
-const OPCode = enum(u8) {
-    // Math
-    Add = 0,
-    Sub,
-    Mul,
-    Div,
-    Inc,
-    Dec,
-    //Control Flow
-    Jmp,
-    Jmpc,
-    //Stack
-    Push,
-    Pop,
-};
-
-const Token = enum(u8) {
-    usingnamespace OPCode;
-    Label = 13,
-    SubLabel,
-    Comptime,
-};
-
-const builtin_token_map = []const []u8{
-    //Math
-    "+",  "-", "*", "/", "++", "--",
-    //Bitop
-    "&",  "|", "^",
-    //Control Flow
-    "!", "?",
-    //Stack
-     "|>",
-    "<|",
-    //Labels
-    ":", ".",
-    //Comptime
-    "@",
-};
-
-pub fn is_builtin(in: []u8, tok: Token) bool {
-    _ = in;
-    _ = tok;
-}
-
 const ReadPageSize = 1024;
 
 pub fn main() !void {
@@ -65,7 +21,6 @@ pub fn main() !void {
     defer fba.reset();
 
     var reader = test_file.reader();
-
     const read: usize = try reader.read(&file_buffer);
 
     std.debug.print("read {d}\n{s}\n", .{ read, &file_buffer });
