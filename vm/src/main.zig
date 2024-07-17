e1c4002a9026344bbfa434228ebecae479812f27
DESKTOP-7J59REQ\meagh <meagherjdavid@gmail.com>
2024-07-17 00:45:51 -0700

    test program

--- vm/src/main.zig
+++ vm/src/main.zig
@@ -1,38 +1,55 @@
 const std = @import("std");
 const vm_lib = @import("vm-lib");
+const isa = @import("./isa.zig");
+const ISA8Bit = isa.ISA8Bit;
 
 usingnamespace vm_lib;
 
 const KB = 1024;
 
 const Registers = enum(u8) {
-    AB = 1,
-    A = 2,
-    B = 3,
-    HL = 4,
-    H = 5,
-    L = 6,
-    PC = 7,
-    SP = 8,
-    BP = 9,
+    AB = 0,
+    A = 1,
+    B = 2,
+    HL = 3,
+    H = 4,
+    L = 5,
+    PC = 6,
+    SP = 7,
+    BP = 8,
 };
 
 fn setup_fn(ctx: *vm_lib.Machine) !void {
     try ctx.registers[@intFromEnum(Registers.A)].set(u8, 0x12);
     try ctx.registers[@intFromEnum(Registers.SP)].set(u8, 0x12);
+    try ctx.load_program(ctx.register_offset, &[_]u8{ 0x00, 0x01, 0x0F });
 }
 
 fn step_fn(ctx: *vm_lib.Machine) !bool {
-    _ = ctx;
-    return false;
+    const pc: *u8 = @ptrCast(ctx.registers[@intFromEnum(Registers.PC)].value.ptr);
+    const byte: u8 = (ctx.memory.read(pc.* + ctx.register_offset, 1) catch return false)[0];
+    pc.* += 1;
+    switch (@as(ISA8Bit, @enumFromInt(byte))) {
+        .Imm => {
+            const register: u8 = (try ctx.memory.read(pc.* + ctx.register_offset, 1))[0];
+            pc.* += 1;
+            const value: u8 = (try ctx.memory.read(pc.* + ctx.register_offset, 1))[0];
+            pc.* += 1;
+            try ctx.registers[register].set(u8, value);
+        },
+        else => {
+            return false;
+        },
+    }
+    return true;
 }
 
 pub fn main() !void {
     var memory_block: [12]u8 = std.mem.zeroes([12]u8);
     const memory: vm_lib.Memory = vm_lib.Memory.init(&memory_block);
     const registers: []vm_lib.Register = @constCast(&[_]vm_lib.Register{
-        vm_lib.Register.Invalid,
         vm_lib.Register{ // AB
+            .is_masking = true,
             .width = 2,
             .value = memory.data[0..2],
         },
@@ -45,6 +62,7 @@ pub fn main() !void {
             .value = memory.data[1..2],
         },
         vm_lib.Register{ // HL
+            .is_masking = true,
             .width = 2,
             .value = memory.data[2..4],
         },
